dd3e92d84cfd8aa07607af64da98ee1bb8bc4b5a
Lucas M. Rocha <lucasmarqueslord@gmail.com>
2025-06-17 13:54:05 -0300

    teste com o .env apontando para mais d eum banco

--- app/log/monitor_remote.py
+++ app/log/monitor_remote.py
@@ -1,17 +1,51 @@
+import os
+import json
 import pyodbc
+from dotenv import load_dotenv
 
-def testar_sql_server(ip, database, user, password, port=1433):
+load_dotenv()
+conexoes = []
+
+total = int(os.getenv("TOTAL_CONEXOES", 0))
+
+for i in range(1, total + 1):
+    tipo = os.getenv(f"CONN{i}_TIPO")
+    host = os.getenv(f"CONN{i}_HOST")
+    port = os.getenv(f"CONN{i}_PORT")
+    db   = os.getenv(f"CONN{i}_DB")
+    user = os.getenv(f"CONN{i}_USER")
+    pwd  = os.getenv(f"CONN{i}_PASS")
+
+    conexoes.append({
+        "tipo": tipo,
+        "host": host,
+        "port": port,
+        "db": db,
+        "user": user,
+        "pwd": pwd
+    })
+for c in conexoes:
+    if c["tipo"] == "sqlserver":
+        testar_sql_server(c["host"], c["db"], c["user"], c["pwd"], c["port"])
+        print(f"Conexão {c['tipo']} com {c['host']} testada.")
+    elif c["tipo"] == "mariadb":
+        testar_mariadb(c["host"], c["db"], c["user"], c["pwd"], c["port"])
+    elif c["tipo"] == "postgres":
+        testar_postgres(c["host"], c["db"], c["user"], c["pwd"], c["port"])
+        
+        
+def testar_sql_server(tipo, ip, db, user, password, porta):
     try:
-        conn = pyodbc.connect(
-            f"DRIVER={{ODBC Driver 18 for SQL Server}};"
-            f"SERVER={ip},{port};"
-            f"DATABASE={database};"
-            f"UID={user};"
-            f"PWD={password};"
-            "Encrypt=no;"  # ou "yes" dependendo da política de segurança
-            "TrustServerCertificate=yes;",
-            timeout=5
-        )
+        # conn = pyodbc.connect(
+        #     f"DRIVER={{ODBC Driver 18 for SQL Server}};"
+        #     f"SERVER={ip},{porta};"
+        #     f"DATABASE={database};"
+        #     f"UID={user};"
+        #     f"PWD={password};"
+        #     "Encrypt=no;"  # ou "yes" dependendo da política de segurança
+        #     "TrustServerCertificate=yes;",
+        #     timeout=5
+        # )
         conn.close()
         print(f"[✓] Conexão OK com {ip}")
         return True
@@ -20,4 +54,5 @@ def testar_sql_server(ip, database, user, password, port=1433):
         return False
 
 # Exemplo de uso
-testar_sql_server("192.168.0.10", "MinhaBase", "usuario_sql", "senha_segura")
+if __name__ == "__main__":
+    pass
---
+++ app/main.py
@@ -0,0 +1,5 @@
+import pyodbc
+
+print("Drivers ODBC disponíveis:")
+for driver in pyodbc.drivers():
+    print(driver)
